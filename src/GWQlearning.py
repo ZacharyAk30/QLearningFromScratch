280713d83111db96ae1bb2ef904dbb153f2bb9b4
zacharyak30 <zachary.akakpo@efrei.net>
2023-11-12 15:48:58 +0100

    update traitement par batch avec un buffer

--- src/GWQlearning.py
+++ src/GWQlearning.py
@@ -6,9 +6,10 @@ import numpy as np
 import random
 
 class GWDeepQlearning:
-    def __init__(self, grid_size, hidden_dim=64, lr=0.05, gamma=0.99,epsilon=0.1):
+    def __init__(self, grid_size, hidden_dim=64,capacity = 100 , lr=0.05, gamma=0.99,epsilon=0.1):
         self.gamma = gamma
         self.grid_size = grid_size
+        self.replay_buffer = ReplayBuffer(capacity)
         self.action_to_int = {
             (0, -1): 0, 
             (0, 1): 1, 
@@ -34,29 +35,54 @@ class GWDeepQlearning:
         self.criterion = nn.MSELoss()
         self.epsilon = epsilon
 
-    def update(self, state:list, action:list, reward:int, next_state:list, done:bool):
-        state = self._process_state(state)
-        next_state = self._process_state(next_state)
-        reward = torch.tensor(reward, dtype=torch.float)
-        action = torch.tensor(self.action_to_int[tuple(action)], dtype=torch.long)
+    def update(self, state:list, action:list, reward:int, next_state:list, done:bool, batch_size:int = 64):
+        # Store the transition in the replay buffer
+        # This includes the current state, action taken, reward received, next state, and whether the episode ended
+        self.replay_buffer.push(state, action, reward, next_state, done)
 
-        if len(state.shape) == 1:
-            state = torch.unsqueeze(state, 0)
-            next_state = torch.unsqueeze(next_state, 0)
-            reward = torch.unsqueeze(reward, 0)
-            action = torch.unsqueeze(action, 0)
-            done = (done, )
+        # Only update the network if we have enough samples in the replay buffer
+        # This is to ensure that we have enough data to learn effectively
+        if len(self.replay_buffer) < batch_size:
+            return
 
-        q_values = self.model(state)
-        next_q_values = self.model(next_state)
+        # Sample a minibatch from the replay buffer
+        # This is a random selection of transitions used to update the network
+        minibatch = self.replay_buffer.sample(batch_size)
 
-        q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+        # Unpack the minibatch
+        # This separates the minibatch into individual components
+        states, actions, rewards, next_states, dones = zip(*minibatch)
+
+        # Process the states and next_states
+        # This converts the states and next states into a format that the network can accept
+        states = torch.stack([self._process_state(state) for state in states])
+        next_states = torch.stack([self._process_state(next_state) for next_state in next_states])
+
+        # Convert the rest to tensors
+        # This converts the actions, rewards, and "done" flags into tensors for processing by the network
+        rewards = torch.tensor(rewards, dtype=torch.float)
+        actions = torch.tensor([self.action_to_int[tuple(action)] for action in actions], dtype=torch.long)
+        dones = torch.tensor(dones, dtype=torch.float)
+
+        # Get the current Q-values by passing the states through the network
+        q_values = self.model(states)
+        # Get the next Q-values by passing the next states through the network
+        next_q_values = self.model(next_states)
+
+        # Get the Q-value of the action taken
+        q_value = q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
+        # Get the maximum Q-value for the next state
         next_q_value = next_q_values.max(1)[0]
-        expected_q_value = reward + self.gamma * next_q_value * (1 - torch.tensor(done, dtype=torch.float))
+        # Calculate the expected Q-value
+        expected_q_value = rewards + self.gamma * next_q_value * (1 - dones)
 
+        # Calculate the loss between the current and expected Q-values
         loss = self.criterion(q_value, expected_q_value.detach())
+        # Zero the gradients before backpropagation
         self.optimizer.zero_grad()
+        # Backpropagate the loss
         loss.backward()
+        # Update the network weights
         self.optimizer.step()
 
     def __call__(self, state):
